ac0a6993bd332997f56c1a741c7e5af1d4f40760
Martzyx <114746510+Martzyx@users.noreply.github.com>
2023-05-24 09:54:47 +0200

    shortcut added for confirm button on beta page

--- content.js
+++ content.js
@@ -1,11 +1,14 @@
 document.addEventListener('keydown', function(event) {
   if (event.key === 'Enter') {
     var saveAndExitbutton = document.querySelector('[click="saveDesign(true)"] > button');
-    var deleteButton = document.querySelector('[click="deleteDesign()"] > button')
+    var deleteButton = document.querySelector('[click="deleteDesign()"] > button');
+    var confirmButton = document.querySelector('[click="$root.uiConfirmConfirm()"] > button');
     if (saveAndExitbutton) {
       saveAndExitbutton.click();
     } else if (deleteButton) {
       deleteButton.click();
+    } else if (confirmButton) {
+      confirmButton.click();
     }
   }
 });
