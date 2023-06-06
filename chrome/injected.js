d67ec89449122ad76d1dbf48d33cd0847bb354e5
Martzyx <114746510+Martzyx@users.noreply.github.com>
2023-06-06 10:06:31 +0200

    cleaner way to look for buttons with loop. also shortcut for proceeding in setup guide flow

--- chrome/injected.js
+++ chrome/injected.js
@@ -11,53 +11,49 @@ function setKeyboardShortcut(shortcut1, enableShortcut1) {
     document.addEventListener("keydown", function (event) {
       if (event.key === shortcut1) {
         console.log("Proceed shortcut pressed: " + shortcut1);
-        var saveAndExitbutton = document.querySelector('[click="saveDesign(true)"] > button');
-        var deleteButton = document.querySelector('[click="deleteDesign()"] > button');
-        var confirmButton = document.querySelector('[click="$root.uiConfirmConfirm()"] > button');
-        var leavePage = document.querySelector('[click="hideUnsavedModal(true)"] > button');
-        var saveCopy = document.querySelector('[click="saveDuplicateDesign(designToDuplicate)"] > button');
-        var nextButton = document.querySelector('[ng-click="selectIndex($index, \'next\')"] > button');
-        var createDesign = document.querySelector('[ng-click="submitEvent(config)"] > button');
-        if (saveAndExitbutton) {
-          console.log("Clicking saveAndExitbutton");
-          saveAndExitbutton.click();
-        } else if (deleteButton) {
-          console.log("Clicking deleteButton");
-          deleteButton.click();
-        } else if (confirmButton) {
-          console.log("Clicking confirmButton");
-          confirmButton.click();
-        } else if (leavePage) {
-          console.log("Clicking leavePage");
-          leavePage.click();
-        } else if (saveCopy) {
-          console.log("Clicking saveCopy");
-          saveCopy.click();
-        } else if (nextButton) {
-          console.log("Clicking nextButton");
-          nextButton.click();
-        } else if (createDesign) {
-          console.log("Clicking create design button");
-          createDesign.click();
+
+        const buttons = [
+          { selector: '[click="saveDesign(true)"] > button', message: 'Clicking saveAndExitbutton' },
+          { selector: '[click="deleteDesign()"] > button', message: 'Clicking deleteButton' },
+          { selector: '[click="$root.uiConfirmConfirm()"] > button', message: 'Clicking confirmButton' },
+          { selector: '[click="hideUnsavedModal(true)"] > button', message: 'Clicking leavePage' },
+          { selector: '[click="saveDuplicateDesign(designToDuplicate)"] > button', message: 'Clicking saveCopy' },
+          { selector: '[ng-click="selectIndex($index, \'next\')"] > button', message: 'Clicking nextButton' },
+          { selector: '[ng-click="submitEvent(config)"] > button', message: 'Clicking create design button' },
+          { selector: '[ng-view] .green.button', message: 'Clicking green button in setup flow' },
+          // Add more buttons here 
+        ];
+
+        for (let button of buttons) {
+          const element = document.querySelector(button.selector);
+          if (element) {
+            console.log(button.message);
+            element.click();
+            break;  // Exit the loop as soon as we find a button that exists
+          }
         }
       }
     });
   }
 }
 
+
 function setKeyboardShortcut2(shortcut2, enableShortcut2) {
   if (enableShortcut2) {
     document.addEventListener("keydown", function (event) {
       if (event.key === shortcut2) {
         console.log("closeModal shortcut pressed: " + shortcut2);
-        var closeModal = document.querySelector('[ng-click="closeModal()"]');
-        var exitDesignCreation = document.querySelector('[ng-click="design.exit()"] > button');
-        if (closeModal) {
-          console.log("Clicking closeModal");
-          closeModal.click();
-        } else if (exitDesignCreation) {
-          console.log("Clicking exit new design flow");
-          exitDesignCreation.click();
+        const buttons = [
+          { selector: '[ng-click="closeModal()"]', message: 'Clicking closeModal' },
+          { selector: '[ng-click="design.exit()"] > button', message: 'Clicking exit new design flow' },
+        ]
+        for (let button of buttons) {
+          const element = document.querySelector(button.selector);
+          if (element) {
+            console.log(button.message);
+            element.click();
+            break;  // Exit the loop as soon as we find a button that exists
+          }
         }
       }
     });
