486000839f85ea435621746277df6aae4cb15094
Martzyx <114746510+Martzyx@users.noreply.github.com>
2023-07-31 09:52:22 +0200

    fixed typo

--- chrome/my-popup/src/Help.jsx
+++ chrome/my-popup/src/Help.jsx
@@ -7,7 +7,7 @@ const Help = () => {
             <HorizontalLine />
             <span>
                 This is the help page for ClerkShortcutPro. We help each other very well and with
-                many things. If u need help, you’ve come to the right place. Lets get you helped
+                many things. If you need help, you’ve come to the right place. Lets get you helped
                 with the helpings that you need. Help yourself by reading through this help page
                 thoroughly and you should be well taken care of.
             </span>
