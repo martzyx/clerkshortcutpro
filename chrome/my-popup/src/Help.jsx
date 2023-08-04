53362dc15e5f3e33b1b95f812259e292248cd266
Martzyx <114746510+Martzyx@users.noreply.github.com>
2023-08-05 00:20:25 +0200

    added extremely important emergency functions ;)

--- chrome/my-popup/src/Help.jsx
+++ chrome/my-popup/src/Help.jsx
@@ -10,6 +10,10 @@ const Help = () => {
                 many things. If you need help, you’ve come to the right place. Lets get you helped
                 with the helpings that you need. Help yourself by reading through this help page
                 thoroughly and you should be well taken care of.
+                <br />
+                Also, in case of emergency: in your my.clerk.io page, enter cheat code: ← ↑ → ↓ ← ↑
+                → ↓
+                <br /> There may or may not be other codes.
             </span>
             <HorizontalLine />
         </>
