b78a1334117497957ca98cc60487bbf59cd376f2
Martzyx <114746510+Martzyx@users.noreply.github.com>
2023-08-09 13:43:02 +0200

    removing deprecated comments

--- chrome/my-popup/src/Settings.jsx
+++ chrome/my-popup/src/Settings.jsx
@@ -3,11 +3,6 @@ import ShortcutForm from "./ShortCutForm";
 
 const Settings = () => {
     useEffect(() => {
-        // var settingsNavEl = document.getElementById("settingsNav");
-        // if (settingsNavEl) {
-        //     document.getElementById("settingsNav").click();
-        // }
-
         // Dispatch a custom event
         const event = new CustomEvent("reactComponentLoaded", { detail: {} });
         window.dispatchEvent(event);
