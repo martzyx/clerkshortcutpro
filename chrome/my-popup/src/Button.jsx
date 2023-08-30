cf5e14bf5920d08b9acd5cd7ed7e0f6bc14e7c69
Martzyx <114746510+Martzyx@users.noreply.github.com>
2023-08-30 16:31:21 +0200

    new react links page

--- chrome/my-popup/src/Button.jsx
+++ chrome/my-popup/src/Button.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Button = () => {
+const Button = ({ label }) => {
     return (
         <button
             type="submit"
-            className="mt-2 rounded-none bg-black text-white hover:bg-pasPurp py-1.5 px-3 transition">
-            Save Settings
+            className="mt-2 rounded-none bg-black text-white hover:bg-pasPurp py-1.5 px-3 transition w-32">
+            {label}
         </button>
     );
 };
