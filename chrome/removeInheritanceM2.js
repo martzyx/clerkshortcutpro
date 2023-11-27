b9ea4a40848a5c05c01ea4eedbe403b7d0b8989e
Martzyx <114746510+Martzyx@users.noreply.github.com>
2023-11-27 20:27:46 +0100

    added functionality to check boxes

--- chrome/removeInheritanceM2.js
+++ chrome/removeInheritanceM2.js
@@ -1,3 +1,4 @@
+//remove checked disabled
 var clerkInputs = document.querySelectorAll('[id^="row_clerk_"]');
 
 clerkInputs.forEach(function (clerkElement) {
@@ -9,6 +10,19 @@ clerkInputs.forEach(function (clerkElement) {
         }
         if (nameElement.hasAttribute("checked")) {
             nameElement.removeAttribute("checked");
+        }
+    });
+});
+
+//add checked disabled (Use website)
+var clerkInputs = document.querySelectorAll('[id^="row_clerk_"]');
+
+clerkInputs.forEach(function (clerkElement) {
+    var nameElements = clerkElement.querySelectorAll('[name*="groups"]');
+
+    nameElements.forEach(function (nameElement) {
+        if (nameElement.classList.contains("checkbox")) {
+            nameElement.setAttribute("checked", "checked");
         }
     });
 });
