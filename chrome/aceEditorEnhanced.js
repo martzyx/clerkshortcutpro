d795a954dae15c1e8a84a5a5e5c89bca8ac5d72d
Martzyx <114746510+Martzyx@users.noreply.github.com>
2023-12-03 18:20:27 +0100

    working version of enlargening editor

--- chrome/aceEditorEnhanced.js
+++ chrome/aceEditorEnhanced.js
@@ -1,53 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // check if iframe is old myclerk
-    if (document.body && window.location.href.startsWith("https://old-my.clerk.io/")) {
-        const observer = new MutationObserver((mutations) => {
-            checkUrl();
-            console.log("first observer run");
-            const element = document.querySelector('[ng-view][autoscroll="true"].ng-scope');
-            if (element.getAttribute("data-csp") == "true") {
-                observer.disconnect();
-            }
-        });
-
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true,
-        });
+function checkEl() {
+    // Check if oldmyclerk iframe
+    if (!window.location.href.startsWith("https://old-my.clerk.io")) {
+        return;
     }
-});
 
-function checkUrl() {
-    if (window.location.href.includes("/code") && window.location.href.startsWith("https://old-my.clerk.io/")) {
-        // Check if the angular element is loaded
-        function checkAngularEl(observer) {
-            const element = document.querySelector('[ng-view][autoscroll="true"].ng-scope');
-            if (element) {
-                enlargeEditor();
-                observer.disconnect();
-                console.log("second observer run");
-            }
-        }
-
-        const config = { attributes: false, childList: true, subtree: true };
-
-        const callback = function (mutationsList, observer) {
-            for (const mutation of mutationsList) {
-                if (mutation.type === "childList") {
-                    checkAngularEl(observer);
-                }
-            }
-        };
-
-        const observer = new MutationObserver(callback);
-        const targetNode = document.body;
-        observer.observe(targetNode, config);
+    let editorWidthEl = document.querySelector('[ng-view][autoscroll="true"].ng-scope');
+    if (editorWidthEl) {
+        enlargeEditor();
+        clearInterval(intervalId);
     }
 }
 
+let intervalId = setInterval(checkEl, 200);
+
 function enlargeEditor() {
     // https://ace.c9.io/#nav=howto
-    const editorWidthEl = document.querySelector('[ng-view][autoscroll="true"].ng-scope');
+    let editorWidthEl = document.querySelector('[ng-view][autoscroll="true"].ng-scope');
     const editorEl = document.querySelectorAll(".editor");
     editorEl.forEach((el) => (el.style = "height:calc(100vh - 5em)"));
 
@@ -56,7 +24,6 @@ function enlargeEditor() {
         let editor = ace.edit(editorID);
         editor.resize();
     }
-
 
     if (editorWidthEl) {
         editorWidthEl.style = "max-width:100vw";
