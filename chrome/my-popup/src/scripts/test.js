48c81ad6ee16f2e5d56db6ddcfe26676ca8e609d
johannes67890 <johannes@orager.dk>
2024-09-12 15:28:10 +0200

    Made new init

--- chrome/my-popup/src/scripts/test.js
+++ chrome/my-popup/src/scripts/test.js
@@ -1,19 +1,18 @@
 const puppeteer = require('puppeteer');
 const axios = require('axios');
 
-function getConfigKey(url) {
-  if (!puppeteer) {
-    console.error('puppeteer is not available in the browser');
-    return;
-  }
+// function getConfigKey(url) {
+  // if (!puppeteer) {
+  //   console.error('puppeteer is not available in the browser');
+  //   return;
+  // }
 
   (async () => {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
-    await page.goto(url); // Replace with your target URL
+    await page.goto("https://www.falkenstenvin.dk/"); // Replace with your target URL
     await page.setBypassCSP(true);
-
     // Run the function `Clerk._config.key` on the webpage and get the result
     const config = await page.evaluate(() => {
       // Make sure Clerk and its properties are available
@@ -22,20 +21,23 @@ function getConfigKey(url) {
       } else {
         return 'Clerk._config is not available.';
       }
-    });
-
-    axios({
-      method: 'get',
-      url: `https://api.clerk.io/v2/client/account/info?key=${config.key}`
     })
-      .then(response => {
-        return response.data;
-      })
-      .catch(error => {
-        return 'API error:', error;
-      });
-
-    await browser.close();
+  
+    console.log('config:', config.key);
   })();
-}
-module.exports = getConfigKey;
+
+  //   axios({
+  //     method: 'get',
+  //     url: `https://api.clerk.io/v2/client/account/info?key=${config.key}`
+  //   })
+  //     .then(response => {
+  //       return response.data;
+  //     })
+  //     .catch(error => {
+  //       return 'API error:', error;
+  //     });
+
+  //   await browser.close();
+  // })();
+// }
+// module.exports = getConfigKey;
