49f871c157a2f9892d921c9b0ff43fc6e68fe0bd
johannes67890 <johannes@orager.dk>
2024-12-20 13:59:26 +0100

    Parsed clerk clients

--- src/extension/webResources/ClerkHQScraper.ts
+++ src/extension/webResources/ClerkHQScraper.ts
@@ -1,24 +1,72 @@
 import DTO from "../../DTO";
 
+ interface Company {
+    id: number;
+    account_id: string;
+    name: string;
+    store: string;
+    enabled: boolean;
+    key: string;
+  }
+  
+   interface Store {
+    id: string;
+    name: string;
+    key: string;
+    client_key: string;
+  }
+  
+   interface User {
+    id: number;
+    account_id: string;
+    name: string;
+    user: {
+      id: number;
+      name: string;
+      email: string;
+      phone: string;
+      profession: string;
+      number_format: string;
+      error_email: boolean;
+      status_email: string;
+      created_at: number;
+      role: string;
+    };
+    store: string;
+    enabled: boolean;
+    key: string;
+  }
+  
+   interface Clients {
+    companies: Company[];
+    stores: Store[];
+    users: User[];
+  }
+  
+  interface ClerkData {
+    status: string;
+    clients: Clients;
+  }
 
+  const { fetch: origFetch } = window;
+  window.fetch = async (...args) => {
+      const response = await origFetch(...args);
+      
+      // If the response of the network request is from the clerk.io API & the request is a list
+      // of clients
 
-const { fetch: origFetch } = window;
-window.fetch = async (...args) => {
-    const response = await origFetch(...args);
-    
-    // If the reqponse of the network request is from the clerk.io API & the request is a list
-    // of clients
-    if(response.url.includes('api.clerk.io') && response.url.match('list')){
-        response
-            .clone()
-            .json()
-            .then(data => {
-                window.postMessage({ type: DTO.HQclerkClients, data: data }, '*'); // send to content script
-                //window.postMessage({ type: 'fetch', data: URL.createObjectURL(data) }, '*'); // if a big media file, can createObjectURL before send to content script
-            })
-            .catch(err => console.error(err));      
-            return response;
-    } 
-    return response;
-};
-
+      if(response.url.includes('api.clerk.io') && response.url.match('list')){
+          response
+              .clone()
+              .json()
+              .then((data: ClerkData) => {
+                  if (data.status === "ok") {
+                    console.log(data.clients);
+                    window.postMessage({ type: DTO.HQclerkClients, data: data.clients }, '*'); // send to content script
+                  }
+              })
+              .catch(err => console.error(err));
+              return response;
+      } 
+      return response;
+  };
